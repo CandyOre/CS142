9681c415696cbecba6a60052459a4159ad3be1c7
CandyOreWin10 <834657460@qq.com>
2022-08-12 01:05:55 +0800

    Add: Proj2 Problem1

---
+++ Proj2_JavaScript/cs142-make-multi-filter.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function cs142MakeMultiFilter (originalArray) {
+    let currentArray = originalArray;
+    const arrayFilterer = function (filterCriteria, callback) {
+        if (typeof filterCriteria !== 'function') {
+            return currentArray;
+        }
+        currentArray = currentArray.filter(filterCriteria);
+        if (typeof callback === 'function') {
+            callback = callback.bind(originalArray);
+            callback(currentArray);
+        }
+        return arrayFilterer;
+    };
+    return arrayFilterer;
+}
